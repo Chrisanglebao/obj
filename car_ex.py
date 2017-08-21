af425822381ae45b2385cad58cefd874a72091df
Sunyang Fu <sunyangfu@gmail.com>
2017-08-21 10:15:25 -0500

    init

---
+++ car_ex.py
@@ -0,0 +1,45 @@
+"""
+    File name: 
+    Project: 
+    Desciption:
+    Author: 
+    Date last modified: 
+"""
+
+from abc import ABCMeta, abstractmethod
+
+class Vehicle(object):
+    """
+    Attributes:
+        wheels: An integer representing the number of wheels the vehicle has.
+        miles: The integral number of miles driven on the vehicle.
+        make: The make of the vehicle as a string.
+        model: The model of the vehicle as a string.
+        year: The integral year the vehicle was built.
+        sold_on: The date the vehicle was sold.
+    """
+
+    __metaclass__ = ABCMeta
+
+    base_sale_price = 0
+
+    def sale_price(self):
+        """Return the sale price for this vehicle as a float amount."""
+        if self.sold_on is not None:
+            return 0.0  # Already sold
+        return 5000.0 * self.wheels
+
+    def purchase_price(self):
+        """Return the price for which we would pay to purchase the vehicle."""
+        if self.sold_on is None:
+            return 0.0  # Not yet sold
+        return self.base_sale_price - (.10 * self.miles)
+
+    @abstractmethod
+    def vehicle_type():
+        """"Return a string representing the type of vehicle this is."""
+        pass
+
+
+v = Vehicle(4, 0, 'Honda', 'Accord', 2014, None)
+
